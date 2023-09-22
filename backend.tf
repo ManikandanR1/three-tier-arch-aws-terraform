8319fdf02ed1ae16157364a5fe44e394b1c2ca62
ManikandanR1 <139693752+ManikandanR1@users.noreply.github.com>
2023-09-22 22:21:52 +0530

    Update backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ajit-inamdar-tech-terraform-backend"
+    bucket         = "mani-tech-terraform-backend-1"
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "ajit-inamdar-tech-terraform-backend"
+    dynamodb_table = "mani-tech-terraform-backend-mani"
   }
 }
